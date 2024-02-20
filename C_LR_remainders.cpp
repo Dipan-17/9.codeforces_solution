3ada6acb08b8b54c25b5afaa70007b2de121d3be
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-02-20 22:39:15 +0530

    another save

---
+++ C_LR_remainders.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+//variable types
+#define ll long long
+#define ulli unsigned long long int
+//declaring data structures
+#define vi vector<int>
+#define vl vector<long long>
+#define mi map<int,int>
+#define ml map<long long,long long>
+#define ppi pair<int,int>
+#define ppl pair<long long,long long>
+#define F first
+#define S second
+//small functions
+#define sa(a,n) sort(a,a+n)
+#define sv(v) sort(v.begin(),v.end())
+#define l(s) s.length()
+//inputs
+#define tn ll n; cin>>n;
+#define pb push_back
+//loops
+#define ff(i,a,b) for(int i=a;i<b;i++)
+#define fb(i,b,a) for(int i=b;i>=a;i--)
+#define w(t) while(--t >= 0)
+//array input output
+#define input(a,n) for(int i=0;i<n;i++) cin>>a[i];
+#define output(a,n) for(int i=0;i<n;i++) cout<<a[i]<<" ";
+//vector input output
+#define readvector(v)for(int i=0;i<v.size();i++)cin>>v[i];
+#define printvector(v)for(int i=0;i<v.size();i++)cout<<v[i]<<" ";
+//output functions
+#define co(n) cout<<n;
+#define cy cout<<"YES\n"
+#define cn cout<<"NO\n"
+#define nl cout<<"\n"
+//constants
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+//modular arithmetic
+long long mod(long long n){return ((n%MOD+MOD)%MOD);}
+long long mul(long long a,long long b){return (mod(mod(a)*mod(b)));}
+long long add(long long a, long long b){return (mod(mod(a)+mod(b)));}
+//mathematical functions
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+bool isPrime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+bool isPerfectSq(ll sum){if(floor(sqrt(sum))== ceil(sqrt(sum)))return true;else return false;}
+
+class Solution {
+public:
+    void solve() {
+       //freopen("input.txt","r", stdin);
+       //freopen("output.txt", "w", stdout);
+        ll n;
+        cin >> n;
+        ll m;
+        cin >> m;
+        vector<ll> arr(n, 0);
+        ff(i,0, n)
+        {
+            cin >> arr[i];
+        }
+        string s;
+        cin >> s;
+        vector<ll> v;
+        ll j = 0, k = n - 1;
+        ff(i,0, n - 1)
+        {
+            if (s[i] == 'L')
+            {
+                j++;
+            }
+            else
+            {
+                k--;
+            }
+        }
+        ll ans = arr[k]%m;
+        v.pb(arr[k] % m);
+        for (ll i = n - 2; i >= 0; i--)
+        {
+            if (s[i] == 'R')
+            {
+                k++;
+                ans = (ans * arr[k]) % m;
+            }
+            else
+            {
+                j--;
+                ans = (ans * arr[j]) % m;
+            }
+            v.pb(ans);
+        }
+        reverse(v.begin(),v.end());
+        for (auto it : v)
+        {
+            cout << it << " ";
+        }
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll tc=1;
+    cin >> tc;
+    for (ll t = 1; t <= tc; t++) {
+        Solution s;
+        s.solve();
+        nl;
+    }
+    return 0;
+}
